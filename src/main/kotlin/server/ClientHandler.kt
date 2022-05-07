4ba135bea1fb5886c194992ff64c2bd5b04c0ccd
deathsmilestome <vanekt35@mail.ru>
2022-05-07 18:37:48 +0300

    +rework ClientHandler.kt

--- src/main/kotlin/server/ClientHandler.kt
+++ src/main/kotlin/server/ClientHandler.kt
@@ -10,9 +10,64 @@ import kotlin.concurrent.thread
 
 
 class ClientHandler(private val client: Socket, private val otherClients: Set<ClientHandler>) {
+
+    companion object {
+        fun write(message: Message, writer: OutputStream) {
+            writer.write((message.checkForFile()).toByteArray().size shr 24)
+            writer.write((message.checkForFile()).toByteArray().size shr 16)
+            writer.write((message.checkForFile()).toByteArray().size shr 8)
+            writer.write((message.checkForFile()).toByteArray().size)
+            writer.write((message.checkForFile()).toByteArray())
+            if (message.file != null) {
+                writer.write(message.file!!)
+            }
+        }
+
+        fun readAll(reader: InputStream): List<String> {
+            var size = 0
+            for (i in 0 until 4 ) {
+                size = size shl 8
+                size += reader.read() and 0xff
+            }
+            val msgBytes = ByteArray(size)
+            for (i in 0 until size) {
+                msgBytes[i] = (reader.read().toByte())
+            }
+            return """:::""".toRegex().split(msgBytes.decodeToString())
+        }
+
+        fun readMessage(message: List<String>, reader: InputStream?): Message {
+            val msg = if (message.size > 3) {
+                Message(message[1], message[2],
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                    message[3], message[4].toInt(), getFile(reader!!, message[4].toInt()))
+            } else {
+                Message(message[1], message[2], LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+            }
+            return msg
+        }
+
+        fun getFile (reader: InputStream, size: Int): ByteArray {
+            val fileBytes = ByteArray(size)
+            for (i in 0 until size) {
+                fileBytes[i] = reader.read().toByte()
+            }
+            return fileBytes
+        }
+
+    }
+
     private var reader: InputStream = client.getInputStream()
     private var running: Boolean = false
     private var nick: String = ""
+
+    private fun writeAll (message: Message) {
+        otherClients.forEach { client ->
+            if (client.nick != this.nick) {
+                write(message, client.client.getOutputStream())
+            }
+        }
+    }
 
     fun run(stream: InputStream) {
         running = true
@@ -24,53 +79,12 @@ class ClientHandler(private val client: Socket, private val otherClients: Set<Cl
         }
     }
 
-    private fun write(message: Message) {
-        otherClients.forEach { client ->
-            if (client.nick != this.nick) {
-                val writer: OutputStream = client.client.getOutputStream()
-                writer.write((message.checkForFile()).toByteArray().size shr 24)
-                writer.write((message.checkForFile()).toByteArray().size shr 16)
-                writer.write((message.checkForFile()).toByteArray().size shr 8)
-                writer.write((message.checkForFile()).toByteArray().size)
-                writer.write((message.checkForFile()).toByteArray())
-                if (message.file != null) {
-                    writer.write(message.file!!)
-                }
-            }
-        }
-    }
-
-    private fun getFile (size: Int): ByteArray {
-        val fileBytes = ByteArray(size)
-        for (i in 0 until size) {
-            fileBytes[i] = reader.read().toByte()
-        }
-        return fileBytes
-    }
-
     private fun read(reader: InputStream) {
-        var msg: Message
         while (running) {
-            var size = 0
-            for (i in 0 until 4 ) {
-                size = size shl 8
-                size += reader.read() and 0xff
-            }
-            val msgBytes = ByteArray(size)
-            for (i in 0 until size) {
-                msgBytes[i] = (reader.read().toByte())
-            }
-            val result = """:::""".toRegex().split(msgBytes.decodeToString())
-            if (result[2].startsWith("|my nick is")) nick = result[1]
-            if (result[2] == "EXIT") shutdown()
-            msg = if (result.size > 3) {
-                Message(result[1], result[2],
-                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-                    result[3], result[4].toInt(), getFile(result[4].toInt()))
-            } else {
-                Message(result[1], result[2], LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-            }
-            write(msg)
+            val result = readAll(reader)
+            if (nick == "") nick = result[2]
+            else if (result[2] == "EXIT") shutdown()
+            else writeAll(readMessage(result, reader))
         }
     }
 
